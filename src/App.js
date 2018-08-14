a84b103a45a6759f03a10fa5386ab8fa056cd58d
Luke Mico <lukemico@me.com>
2018-08-15 08:05:44 +1000

    update app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 import './App.css';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from "react-apollo";
-
+import Pusher from 'pusher-js';
 
 import Header from './components/Header';
 import Post from './components/Post';
@@ -15,17 +15,30 @@ const client = new ApolloClient({
 
 
 
-const App = () => {
-      return (
-        <ApolloProvider client={client}>
-          <div className="App">
-            <Header />
-            <section className="App-main">
-              <Post />
-            </section>
-          </div>
-        </ApolloProvider>
-      );
-    };
-    export default App;
+// create component
+    class App extends Component{
+      constructor(){
+        super();
+        // connect to pusher
+        this.pusher = new Pusher("9f8edadd86537d115da2", {
+         cluster: 'eu',
+         encrypted: true
+        });
+      }
 
+      render(){
+        return (
+          <ApolloProvider client={client}>
+            <div className="App">
+              <Header />
+              <section className="App-main">
+                {/* pass the pusher object and apollo to the posts component */}
+                <Posts pusher={this.pusher} apollo_client={client}/>
+              </section>
+            </div>
+          </ApolloProvider>
+        );
+      }
+    }
+
+    export default App;
